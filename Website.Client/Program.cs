54be89378fc1aaa00385e1abf1f94569cb7e7893
Billy Mumby <billy.mumby@outlook.com>
2021-08-12 16:56:17 +0100

    Updated to .net 6 preview and added meta headers.

--- Website.Client/Program.cs
+++ Website.Client/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Website.Client.Services;
@@ -13,6 +14,7 @@ namespace Website.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
             builder.Services.AddSingleton<IPostService, PostService>();
