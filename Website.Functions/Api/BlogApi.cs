346846080b61959800a3e90a7689b0921d0e4bde
Billy Mumby <billy.mumby@outlook.com>
2021-08-17 20:55:54 +0100

    Made posts editable to authorized users & updated post display to be full page.

--- Website.Functions/Api/BlogApi.cs
+++ Website.Functions/Api/BlogApi.cs
@@ -32,8 +32,8 @@ namespace Website.Functions.Api
             _techTagsRepository = techTagsRepository;
         }
 
-        [FunctionName("get-post")]
-        public async Task<IActionResult> Get(
+        [FunctionName("published-post")]
+        public async Task<IActionResult> GetPublished(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "blog/{id}")]
             HttpRequestMessage request,
             string id,
@@ -58,23 +58,12 @@ namespace Website.Functions.Api
                 return NotFound();
             }
         }
+
         
-        [FunctionName("get-all-blog-posts")]
-        public async Task<IActionResult> GetBlogPosts(
-            [HttpTrigger(AuthorizationLevel.Function, "get", Route = "owner/blog")]
-            HttpRequestMessage request,
-            ILogger log
-        )
-        {
-            var post = (await _postRepository.GetAsync(p => p.Type == nameof(Post))).ToList();
-            log.LogInformation($"{post.Count()} un-published posts found");
-
-            return Ok(post);
-        }
 
 
-        [FunctionName("published-posts")]
-        public async Task<IActionResult> GetPublished(
+        [FunctionName("all-published-posts")]
+        public async Task<IActionResult> GetAllPublished(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = "blog")]
             HttpRequestMessage request,
             ILogger log
@@ -84,58 +73,6 @@ namespace Website.Functions.Api
             log.LogInformation($"{post.Count()} published posts found");
 
             return Ok(post);
-        }
-
-
-        [FunctionName("create-post")]
-        public async Task<IActionResult> Create(
-            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "owner/blog")]
-            HttpRequestMessage request,
-            ILogger log
-        )
-        {
-            var post = await request.Content.ReadAsAsync<Post>();
-
-            if (string.IsNullOrWhiteSpace(post.Id))
-                return BadRequest("A post must have a unique id.");
-
-            if (string.IsNullOrWhiteSpace(post.Title))
-                return BadRequest("A post must have a title.");
-
-            if (string.IsNullOrWhiteSpace(post.Description))
-                return BadRequest("A post must have a description");
-            
-            if (string.IsNullOrWhiteSpace(post.GistUrl))
-                return BadRequest("A gist url must be provided");
-
-            if (post.Tags.Any(t => string.IsNullOrWhiteSpace(t.Id)))
-                return BadRequest("All tech tags must have an id provided");
-            
-            //TODO: add an exists check for the id used. Wait on PR to be merged and released
-
-            var uri = new Uri(post.GistUrl);
-            var gistId = uri.Segments[^1];
-
-            Gist gist;
-
-            try
-            {
-                gist = await _gitHubClient.Gist.Get(gistId);
-            }
-            catch (ApiException e)
-            {
-                return BadRequest(e.ApiError.Message);
-            }
-
-            if (string.IsNullOrWhiteSpace(post.GistFile) is false && gist.Files.ContainsKey(post.GistFile) is false)
-                return BadRequest($"The gist with id {post.Id} does not contain a file named {post.GistFile}");
-
-            foreach (var postTag in post.Tags)
-                await _techTagsRepository.UpdateAsync(postTag);
-
-            await _postRepository.CreateAsync(post);
-
-            return Ok();
         }
         
     }
